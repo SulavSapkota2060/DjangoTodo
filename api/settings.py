57068c3b4b402f8614f97cb857eb1c88c75cf8fe
Sulav Sapkota <sulavsapkota100@gmail.com>
2020-09-13 21:28:47 +0545

    Ok

--- api/settings.py
+++ api/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
